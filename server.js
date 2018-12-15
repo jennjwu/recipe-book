a775966c093df7e4807e9fc0dcb7de73dca407ac
Jennifer Wu <jenn.j.wu@gmail.com>
2018-12-15 09:27:31 -0800

    Add graphql

--- server.js
+++ server.js
@@ -3,11 +3,41 @@
 const PORT = process.env.PORT || 3000;
 var express = require("express");
 var app = express();
+var graphqlHTTP = require('express-graphql');
+var { graphql, buildSchema } = require('graphql');
+
+
+// Graphql
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
 
 app.get("/", function (req, res) {
     logRequest(req, null);
+
+    // Run the GraphQL query '{ hello }' and print out the response
+    graphql(schema, '{ hello }', root).then((response) => {
+      console.log(response);
+    });
+
     res.json("hello world");
 });
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
 
 // helper to log requests to server
 function logRequest(req, body) {
